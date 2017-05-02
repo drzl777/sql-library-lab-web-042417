60bfcd47f4db0049d5f85a5c58eef01e75b037da
drzly777 <andrew.zheng725@gmail.com>
2017-05-02 13:46:28 -0400

    figured out error messages forgetting to create foregin key before declaring

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,20 @@
+
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
 CREATE TABLE series (
-  id INTEGER PRIMARY KEY
-)
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER,
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (subgenre_id) REFERENCES subgenres(id)
+);
